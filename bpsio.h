70f288d1d9e1bb37f48ef259de02e875bb1d9a13
bps <plankton6664@gmail.com>
2026-05-31 17:42:11 -0400

    init library

---
+++ bpsio.h
@@ -0,0 +1,66 @@
+#ifndef BPSIO_H
+#define BPSIO_H
+
+void bpsio_hide_cursor(void);
+void bpsio_show_cursor(void);
+void bpsio_clear_screen(void);
+int bpsio_getch(void);
+
+#ifndef BPSIO_NO_SHORT_NAMES
+#define hide_cursor() bpsio_hide_cursor()
+#define show_cursor() bpsio_show_cursor()
+#define clear_screen() bpsio_clear_screen()
+#define getch() bpsio_getch()
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BPSIO_H */
+
+// =================================
+
+#ifdef BPSIO_IMPLEMENTATION
+
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+void bpsio_hide_cursor(void) {
+  printf("\033[?25l");
+  fflush(stdout);
+}
+
+void bpsio_show_cursor(void) {
+  printf("\033[?25h");
+  fflush(stdout);
+}
+
+void bpsio_clear_screen(void) {
+  printf("\033[H\033[2J");
+  fflush(stdout);
+}
+
+int bpsio_getch(void) {
+  struct termios old_attr, new_attr;
+  int ch;
+
+  if (tcgetattr(STDIN_FILENO, &old_attr) == -1) {
+    return -1;
+  }
+
+  new_attr = old_attr;
+  new_attr.c_lflag &= ~(ICANON | ECHO);
+
+  if (tcsetattr(STDIN_FILENO, TCSANOW, &new_attr) == -1) {
+    return -1;
+  }
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &old_attr);
+  return ch;
+}
+
+#endif /* BPSIO_IMPLEMENTATION */
